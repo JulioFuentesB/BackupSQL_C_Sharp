986533f2cabd2bfdeb71046830cfd1c7e177b34a
JulioFuentesBarrera <JulioFuentesBarrera@101061PC>
2021-07-08 11:54:22 -0500

    Se sube proyecto respaldo bk de sql express

---
+++ BackupSQL_C_Sharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupSQL_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Starting process...");
+            Backup backup = new Backup();
+            backup.BackupDatabase();
+        }
+    }
+}
